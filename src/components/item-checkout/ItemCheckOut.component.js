702392a370ee6144a3c4884f1552bfc0c08fcbc4
Daniel Mason <danmasson0@gmail.com>
2021-10-28 01:22:22 -0400

    add addItem and removeItem functionality to checkout item component

--- src/components/item-checkout/ItemCheckOut.component.js
+++ src/components/item-checkout/ItemCheckOut.component.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { clearItemFromCart, addItem, removeItem  } from '../../redux/cart/cart.actions';
 
-const ItemCheckOut = ({ cartItem: { name, imageUrl, price, quantity}}) => {
+const ItemCheckOut = ({ cartItem, clearItem, addItem, removeItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
     return (
         <StyledCheckOutItem>
-            <ImageContainer>
+            <div className="container-image">
                 <img src={imageUrl} alt="item" />
-            </ImageContainer>
+            </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+            <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+            <span className="value">{quantity}</span>
+            <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
             <span className="price">{price}</span>
-            <div className="remove-button">&#10005;</div>
+            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
         </StyledCheckOutItem>
     )
 }
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
 
 
 const StyledCheckOutItem = styled.div `
@@ -25,6 +38,17 @@ const StyledCheckOutItem = styled.div `
     padding: 15px 0;
     font-size: 20px;
     align-items: center;
+
+    .container-image{
+
+        width: 23%;
+      padding-right: 15px;
+  
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
   
     
     .name,
@@ -34,7 +58,15 @@ const StyledCheckOutItem = styled.div `
     }
   
     .quantity {
-      padding-left: 20px;
+      display: flex;
+      
+      .arrow{
+          cursor: pointer;
+      }
+
+      .value{
+          margin: 0 10px;
+      }
     }
   
     .remove-button {
@@ -43,14 +75,4 @@ const StyledCheckOutItem = styled.div `
     }
 `;
 
-const ImageContainer = styled.img`
-      width: 23%;
-      padding-right: 15px;
-  
-      img {
-        width: 100%;
-        height: 100%;
-      }
-`;
-
-export default ItemCheckOut;
+export default connect(null, mapDispatchToProps)(ItemCheckOut);
