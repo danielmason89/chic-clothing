5125b5cb911f1f0c3384192ce6212ca5e5bc56d8
Daniel Mason <danmasson0@gmail.com>
2021-11-22 09:36:47 -0500

    update code to be pwa compliant, added serviceWorker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // Redux
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor }from './redux/Store';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/Store";
 // Chic App
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+// Service Worker
+import * as serviceWorker from "./service-worker";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <React.StrictMode>
-    <PersistGate persistor={persistor}>
-    <App />
-    </PersistGate>
-    </React.StrictMode>
+      <React.StrictMode>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </React.StrictMode>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
